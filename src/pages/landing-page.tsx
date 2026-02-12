6b089cb76144218861bc55638ae3fba2b33abace
Eduardo Talavera Ramos <eduardotalaverar1@gmail.com>
2026-02-12 13:31:28 -0700

    small details landing page

--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,3 +1,4 @@
+import { images } from "../seeds/images";
 const mainImg = "https://images.unsplash.com/photo-1770105328550-b0e90d770c17?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 function Header() {
@@ -16,16 +17,33 @@ function Header() {
                     </h2>
                 </section>
                 <section className="grid grid-cols-10 gap-2 w-full h-[90vh]">
-                    {/* hacer componente */}
-                    <div className="col-span-4 bg-white"></div>
-                    <div className="col-span-2 bg-white"></div>
-                    <div className="col-span-4 bg-white"></div>
-                    <div className="col-span-2 bg-white"></div>
-                    <div className="col-span-3 bg-white"></div>
-                    <div className="col-span-5 bg-white"></div>
-                    <div className="col-span-3 bg-white"></div>
-                    <div className="col-span-5 bg-white"></div>
-                    <div className="col-span-2 bg-white"></div>
+                    <div className="col-span-4 bg-white h-[40vh]">
+                        <img src={images[0]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-2 h-[40vh]">
+                        <img src={images[1]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-4 h-[40vh]">
+                        <img src={images[2]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-2 h-[40vh]">
+                        <img src={images[3]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-3 h-[40vh]">
+                        <img src={images[4]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-5  h-[40vh]">
+                        <img src={images[5]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-3  h-[40vh]">
+                        <img src={images[6]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-5  h-[40vh]">
+                        <img src={images[7]} alt="" className="object-cover h-full w-full" />
+                    </div>
+                    <div className="col-span-2  h-[40vh]">
+                        <img src={images[8]} alt="" className="object-cover h-full w-full" />
+                    </div>
                 </section>
             </main>
         </>
---
+++ src/seeds/images.ts
@@ -0,0 +1,11 @@
+export const images = [
+    "https://images.unsplash.com/photo-1770768061898-6d5c4f5d77a5?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770816307489-86c2d684ace7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770872686537-11a75a02c1cc?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770836037330-9184ab261ed0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770643770962-7937094987a9?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770753105322-fdca548966b0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770230901490-a9e30b2f3e75?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770031079091-c6356e82ea9b?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770297346253-f1255dd167dc?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
